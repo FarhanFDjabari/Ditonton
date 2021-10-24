f1ee9860d47c00aa708edcda4c0dbfd9c8dad206
FarhanFDjabari <ffadhilah8@gmail.com>
2021-10-24 20:22:17 +0800

    Fix Testing Error

--- tv_series/test/helpers/test_helper.dart
+++ tv_series/test/helpers/test_helper.dart
@@ -2,10 +2,9 @@ import 'package:core/data/datasources/db/database_helper.dart';
 import 'package:core/utils/network_info.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/annotations.dart';
-
-import '../../lib/data/datasources/tv_series_local_data_source.dart';
-import '../../lib/data/datasources/tv_series_remote_data_source.dart';
-import '../../lib/domain/repositories/tv_series_repository.dart';
+import 'package:tv_series/data/datasources/tv_series_local_data_source.dart';
+import 'package:tv_series/data/datasources/tv_series_remote_data_source.dart';
+import 'package:tv_series/domain/repositories/tv_series_repository.dart';
 
 @GenerateMocks([
   TvSeriesRepository,
